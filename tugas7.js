f1f3eefe56837d23396f8023e84a6c288f7828df
Muhamad Fatah <muhamad.fatah494@gmail.com>
2022-05-19 16:38:52 +0700

    tugas 7 js
    tugas 7 js - niomic

---
+++ tugas7.js
@@ -0,0 +1,10 @@
+var tinggi = [150, 155, 156, 159, 160, 165, 170, 175, 177, 180];
+for (i=0; i<tinggi.length; i++){
+    console.log(tinggi[i])
+}
+
+console.log("")
+
+for (let x of tinggi){
+    console.log(x);    
+}
